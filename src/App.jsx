13effc2c73439ce8eee4925bf7dc127cdb1aaeb5
nehavaghasiya1502 <nehavaghasiya573@gmail.com>
2025-12-03 17:48:11 +0530

    updated files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './component/Header';
+import Footer from './component/Footer';
+
+const App = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default App
